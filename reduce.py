37268d61718d5b1d8644cba2c9db9a086c7cacce
Miguel de Val-Borro <miguel.deval@gmail.com>
2013-07-16 15:55:26 -0400

    average folded spectra

--- reduce.py
+++ reduce.py
@@ -6,6 +6,22 @@ import numpy as np
 from os.path import expanduser, join
 import glob
 import argparse
+from scipy import interpolate
+from hsso import gildas
+
+def ff(pol):
+    hdulist = pyfits.open( glob.glob(
+            join(datadir, str(obsid), 'level2',
+            '{0}-{1}-{2}'.format(args.backend, pol, args.sideband),
+            'box_001', '*.fits*'))[0])
+
+    for i in hdulist[1].header.ascardlist().keys():
+        if hdulist[1].header[i] == 'loThrow':
+            throw = hdulist[1].header[i[4:]]
+            break
+    freq = hdulist[1].data.field('{0}frequency_{1}'.format(args.sideband.lower(), args.subband))[0]
+    flux = hdulist[1].data.field('flux_{0}'.format(args.subband))[0]
+    return freq, flux, throw
 
 # Parsing command line arguments
 parser = argparse.ArgumentParser()
@@ -18,20 +34,14 @@ datadir = expanduser('~/HssO/Christensen/data')
 obsid = 1342204014
 freq0 = 556.9359877
 
-for pol in ('H', 'V'):
-    hdulist = pyfits.open( glob.glob(
-            join(datadir, str(obsid), 'level2',
-            '{0}-{1}-{2}'.format(args.backend, pol, args.sideband),
-            'box_001', '*.fits*'))[0])
+freqh, fluxh, throwh = ff('H')
+freqv, fluxv, throwv = ff('V')
 
-    for i in hdulist[1].header.ascardlist().keys():
-        if hdulist[1].header[i] == 'loThrow':
-            throw = hdulist[1].header[i[4:]]
-            break
+freq_list = (freqh, freqh+throwh, freqv, freqv+throwv)
+flux_list = (fluxh, -fluxh, fluxv, -fluxv)
 
-    freq = hdulist[1].data.field('{0}frequency_{1}'.format(args.sideband.lower(), args.subband))[0]
-    flux = hdulist[1].data.field('flux_{0}'.format(args.subband))[0]
-    plt.plot(freq, flux, drawstyle='steps-mid')
-    plt.plot(freq+throw, -flux, drawstyle='steps-mid')
+freqav, fluxav = gildas.averagen(freq_list, flux_list)
+
+plt.plot(freqav, fluxav,  drawstyle='steps-mid')
 plt.axvline(x=freq0, linestyle='--')
 plt.show()
