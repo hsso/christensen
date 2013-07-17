a3974470f1dcac7912121dcf9ffa8c37f644db30
Miguel de Val-Borro <miguel.deval@gmail.com>
2013-07-17 18:06:39 -0400

    plot comet trayectory

--- pacs.py
+++ pacs.py
@@ -6,8 +6,9 @@ import numpy as np
 from os.path import join
 import glob
 import pywcsgrid2
+import pywcs
 import argparse
-from christensen import datadir
+from christensen import *
 import matplotlib.cm as cm
 
 parser = argparse.ArgumentParser()
@@ -23,12 +24,17 @@ for c in ('Blue', 'Red'):
     sh = pmap.shape
     patch = pmap[sh[0]/2-5:sh[0]/2+5, sh[1]/2-5:sh[1]/2+5]
     h = hdulist[1].header
+    wcs = pywcs.WCS(hdulist[1].header)
+    comet = wcs.wcs_sky2pix([radec1, radec2], 1)
+    print hdulist[0].header['DATE-OBS']
 
-#     ax = pywcsgrid2.subplot(111, header= h)
-#     ax.set_ticklabel_type("delta", "delta", offset=h["crval1"],
-#                     latitude=h["crval2"])
+    ax = pywcsgrid2.subplot(111, header= h)
+    ax.set_ticklabel_type("delta", "delta", offset=radec[0],
+                    latitude=radec[1])
 #     ax.grid()
-    plt.imshow(patch, origin="lower", cmap=cm.gist_heat_r)
-    plt.colorbar()
-    plt.contour(np.log10(patch), colors='k')
+    plt.imshow(pmap, origin="lower", cmap=cm.gist_heat_r)
+#     plt.plot(ra, dec, 'ro-')
+    for i in comet: plt.scatter(*i)
+#     plt.colorbar()
+#     plt.contour(np.log10(patch), colors='k')
     plt.show()
