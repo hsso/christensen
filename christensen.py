0b21c3bf590df663608514247c2a76b6bf90ca8a
Miguel de Val-Borro <miguel.deval@gmail.com>
2013-08-12 11:47:09 -0400

    add Christensen data module

---
+++ christensen.py
@@ -0,0 +1,10 @@
+
+from os.path import expanduser
+
+datadir = expanduser('~/HssO/Christensen/data')
+freq0 = {'H2O': 556.9359877, 'NH3': 572.4980678}
+
+radec1 = (287.58229, -9.10377)
+radec2 = (287.58256, -9.10535)
+
+radec = (287.58242,  -9.10456)
