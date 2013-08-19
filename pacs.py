97cbe3d951413473f346ccf54190507f8f755add
Miguel de Val-Borro <miguel.deval@gmail.com>
2013-08-19 13:23:22 -0400

    show obsid and filter in title

--- pacs.py
+++ pacs.py
@@ -7,7 +7,7 @@ from os.path import join
 import glob
 import pywcs
 import argparse
-from christensen import *
+from christensen import datadir, radec
 import matplotlib.cm as cm
 
 parser = argparse.ArgumentParser()
@@ -22,16 +22,17 @@ for c in ('Blue', 'Red'):
         'HPPPMAP{}'.format(c[0]), '*fits.gz'))[0]
     hdulist = pyfits.open(fitsfile)
 
+    # swap to little-endian byte order to avoid matplotlib bug
     pmap = hdulist[1].data.byteswap().newbyteorder()
     cdelt2 = hdulist[1].header['CDELT2']
     date_obs = hdulist[0].header['DATE-OBS']
     wcs = pywcs.WCS(hdulist[1].header)
     comet = wcs.wcs_sky2pix([radec], 1)[0]
-    c = [int(i) for i in comet]
     pix = np.abs(cdelt2)*3600
     fov = int(round(60/pix))
     print fov, date_obs
-#     patch = pmap[c[1]-fov:c[1]+fov, c[0]-fov:c[0]+fov]
+    com = [int(i) for i in comet]
+#     patch = pmap[com[1]-fov:com[1]+fov, com[0]-fov:com[0]+fov]
 
     med = np.std(pmap[pmap > 0.])
     signal_stdev= np.std(pmap[pmap > med])
@@ -44,6 +45,7 @@ for c in ('Blue', 'Red'):
 #     plt.scatter(*comet)
 #     plt.scatter(fov, fov)
     plt.colorbar()
+    plt.title('{0} {1}'.format(args.obsid, c))
 #     plt.plot(ra, dec, 'ro-')
 #     plt.contour(np.log10(patch), colors='k')
     plt.show()
