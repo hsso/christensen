2bf8b0e84519264b8ae3d91141dbd7bcf5f0ca20
Miguel de Val-Borro <miguel.deval@gmail.com>
2013-08-19 20:14:25 -0400

    select PACS filter band

--- christensen.py
+++ christensen.py
@@ -4,7 +4,8 @@ from os.path import expanduser
 datadir = expanduser('~/HssO/Christensen/data')
 freq0 = {'H2O': 556.9359877, 'NH3': 572.4980678}
 
+# ra and dec for 1342186621
 radec1 = (287.58229, -9.10377)
 radec2 = (287.58256, -9.10535)
 
-radec = (287.58242,  -9.10456)
+radec = (287.58242, -9.10456)
--- pacs.py
+++ pacs.py
@@ -14,42 +14,44 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--obsid', default=1342186621, type=int,
                 choices=(1342186621, 1342186622, 1342203478, 1342203479))
 parser.add_argument('-d', '--debug', action='store_true', help='debug mode')
+parser.add_argument('-b', '--band', default="blue", choices=("blue", "red"),
+                help="PACS band")
 args = parser.parse_args()
 
 # pmap[pmap < cutlevels[i][0]] = cutlevels[i][0]
 
-for c in ('Blue', 'Red'):
-    fitsfile = glob.glob(join(datadir, str(args.obsid), 'level2',
-        'HPPPMAP{}'.format(c[0]), '*fits.gz'))[0]
-    hdulist = pyfits.open(fitsfile)
+fitsfile = glob.glob(join(datadir, str(args.obsid), 'level2',
+    'HPPPMAP{}'.format(args.band[0].upper()), '*fits.gz'))[0]
+hdulist = pyfits.open(fitsfile)
 
-    # swap to little-endian byte order to avoid matplotlib bug
-    pmap = hdulist[1].data.byteswap().newbyteorder()
-    cdelt2 = hdulist[1].header['CDELT2']
-    date_obs = hdulist[0].header['DATE-OBS']
-    wcs = pywcs.WCS(hdulist[1].header)
-    comet = wcs.wcs_sky2pix([radec], 1)[0]
-    pix = np.abs(cdelt2)*3600
-    fov = int(round(60/pix))
-    print fov, date_obs
-    com = [int(i) for i in comet]
+# swap to little-endian byte order to avoid matplotlib bug
+pmap = hdulist[1].data.byteswap().newbyteorder()
+cdelt2 = hdulist[1].header['CDELT2']
+date_obs = hdulist[0].header['DATE-OBS']
+wcs = pywcs.WCS(hdulist[1].header)
+# origin coordinate is 0 (Numpy and C standards)
+comet = wcs.wcs_sky2pix([radec], 0)[0]
+pix = np.abs(cdelt2)*3600
+fov = int(round(60/pix))
+print fov, date_obs
+com = [int(i) for i in comet]
 #     patch = pmap[com[1]-fov:com[1]+fov, com[0]-fov:com[0]+fov]
 
-    med = np.std(pmap[pmap > 0.])
-    signal_stdev= np.std(pmap[pmap > med])
-    print med, signal_stdev
+med = np.std(pmap[pmap > 0.])
+signal_stdev= np.std(pmap[pmap > med])
+print med, signal_stdev
 
 #     plt.hist(pmap)
 #     pmap[pmap > 0.1] = 0.1
 #     pmap[pmap < 0.003] = -0.003
-    if args.debug:
-        plt.plot(pmap.flat)
-        plt.show()
-    plt.imshow(pmap, origin="lower", cmap=cm.gist_heat_r)
-#     plt.scatter(*comet)
+if args.debug:
+    plt.plot(pmap.flat)
+    plt.show()
+plt.imshow(pmap, origin="lower", cmap=cm.gist_heat_r)
+plt.colorbar()
+plt.scatter(*comet)
 #     plt.scatter(fov, fov)
-    plt.colorbar()
-    plt.title('{0} {1}'.format(args.obsid, c))
+plt.title('{0} {1}'.format(args.obsid, args.band))
 #     plt.plot(ra, dec, 'ro-')
 #     plt.contour(np.log10(patch), colors='k')
-    plt.show()
+plt.show()
