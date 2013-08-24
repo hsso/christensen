61c458388c685f3f7903d9916ac8c6ed1fc51ba7
Miguel de Val-Borro <miguel.deval@gmail.com>
2013-08-24 19:58:42 -0400

    plot radial profile

--- pacs.py
+++ pacs.py
@@ -9,7 +9,7 @@ import pywcs
 import argparse
 from christensen import datadir, figsdir, horizons_file
 import matplotlib.cm as cm
-from scipy.ndimage import interpolation, zoom
+from scipy import ndimage
 from datetime import datetime
 from hsso import gildas
 
@@ -22,52 +22,68 @@ parser.add_argument('-b', '--band', default="blue", choices=("blue", "red"),
 parser.add_argument('-s', '--save', default="store_true", help="save image")
 args = parser.parse_args()
 
-# pmap[pmap < cutlevels[i][0]] = cutlevels[i][0]
+class pacsmap(object):
+    def __init__(self, obsid):
+        """return patch centered on the nucleus"""
+        self.fitsfile = glob.glob(join(datadir, str(obsid), 'level2',
+            'HPPPMAP{}'.format(args.band[0].upper()), '*fits.gz'))[0]
+        self.hdus = pyfits.open(self.fitsfile)
+        self.cdelt2 = self.hdus[1].header['CDELT2']
 
-def pacsmap(obsid):
-    fitsfile = glob.glob(join(datadir, str(obsid), 'level2',
-        'HPPPMAP{}'.format(args.band[0].upper()), '*fits.gz'))[0]
-    hdus = pyfits.open(fitsfile)
+    def patch(self):
+        # swap to little-endian byte order to avoid matplotlib bug
+        pmap = self.hdus[1].data.byteswap().newbyteorder()
+        date_obs = self.hdus[0].header['DATE-OBS']
+        date_end = self.hdus[0].header['DATE-END']
+        # calculate comet position at midtime
+        start = datetime.strptime(date_obs[:20], "%Y-%m-%dT%H:%M:%S.")
+        end = datetime.strptime(date_end[:20], "%Y-%m-%dT%H:%M:%S.")
+        mid_time = start + (end-start)/2
+        # interpolate ra and dec of the comet
+        ra = gildas.deltadot(mid_time, filename=horizons_file[args.obsid], column=2)
+        dec = gildas.deltadot(mid_time, filename=horizons_file[args.obsid], column=3)
+        # calculate direction toward the Sun
+        phase_ang = gildas.deltadot(mid_time, filename=horizons_file[args.obsid], column=8)
+        alpha = np.pi/2 + phase_ang*np.pi/180
+        cos, sin = np.cos(alpha), np.sin(alpha)
+        print("\draw[->,yellow] (axis cs:{0:.3f},{1:.3f}) -- \n"
+                "(axis cs:{2:.3f},{3:.3f});".format(10*cos, 10*sin, 20*cos, 20*sin))
+        print(r"\node[yellow] at (axis cs:{0:.3f},{1:.3f}) {{\sun}};".format(25*cos,
+                        25*sin))
+        # origin coordinate is 0 (Numpy and C standards)
+        wcs = pywcs.WCS(self.hdus[1].header)
+        comet = wcs.wcs_sky2pix([(ra, dec)], 0)[0]
+        if args.debug:
+            plt.imshow(pmap, origin="lower")
+            plt.scatter(*comet)
+            # plot max position
+            mapmax = np.unravel_index(np.argmax(pmap), pmap.shape)[::-1]
+            plt.scatter(*mapmax)
+            # plot center-of-mass
+            mapcom = ndimage.measurements.center_of_mass(pmap)[::-1]
+            plt.scatter(*mapcom, color='r')
+            plt.show()
+            plt.close()
+        com = [int(round(i)) for i in comet[::-1]]
+        sh  = [i - round(i) for i in comet[::-1]]
+        pmap = ndimage.interpolation.shift(pmap, sh)
+        pix = np.abs(self.cdelt2)*3600
+        fov = int(round(30/pix))
+        print phase_ang, pix, comet, sh, date_obs
+        patch = pmap[com[0]-fov:com[0]+fov+1, com[1]-fov:com[1]+fov+1]
+        if args.obsid == 1342186621: patch = ndimage.zoom(patch, 3, order=2)
+        return patch
 
-    # swap to little-endian byte order to avoid matplotlib bug
-    pmap = hdus[1].data.byteswap().newbyteorder()
-    cdelt2 = hdus[1].header['CDELT2']
-    date_obs = hdus[0].header['DATE-OBS']
-    date_end = hdus[0].header['DATE-END']
-    # calculate comet position at midtime
-    start = datetime.strptime(date_obs[:20], "%Y-%m-%dT%H:%M:%S.")
-    end = datetime.strptime(date_end[:20], "%Y-%m-%dT%H:%M:%S.")
-    mid_time = start + (end-start)/2
-    # interpolate ra and dec of the comet
-    ra = gildas.deltadot(mid_time, filename=horizons_file[args.obsid], column=2)
-    dec = gildas.deltadot(mid_time, filename=horizons_file[args.obsid], column=3)
-    phase_ang = gildas.deltadot(mid_time, filename=horizons_file[args.obsid], column=8)
-    wcs = pywcs.WCS(hdus[1].header)
-    alpha = np.pi/2 + phase_ang*np.pi/180
-    cos, sin = np.cos(alpha), np.sin(alpha)
-    print("\draw[->,yellow] (axis cs:{0:.3f},{1:.3f}) -- \n"
-            "(axis cs:{2:.3f},{3:.3f});".format(10*cos, 10*sin, 20*cos, 20*sin))
-    print(r"\node[yellow] at (axis cs:{0:.3f},{1:.3f}) {{\sun}};".format(25*cos,
-                    25*sin))
-    # origin coordinate is 0 (Numpy and C standards)
-    comet = wcs.wcs_sky2pix([(ra, dec)], 0)[0]
-    if args.debug:
-        plt.imshow(pmap, origin="lower")
-        plt.scatter(*comet)
-        plt.show()
-        plt.close()
-    com = [int(round(i)) for i in comet[::-1]]
-    sh  = [i - round(i) for i in comet[::-1]]
-    pmap = interpolation.shift(pmap, sh)
-    pix = np.abs(cdelt2)*3600
-    fov = int(round(30/pix))
-    print phase_ang, pix, comet, sh, date_obs
-    patch = pmap[com[0]-fov:com[0]+fov+1, com[1]-fov:com[1]+fov+1]
-    if args.obsid == 1342186621: patch = zoom(patch, 3, order=2)
-    return patch
+def radprof(pmap):
+    y, x = np.indices(pmap.shape)
+    j, i = np.unravel_index(np.argmax(pmap), pmap.shape)
+    r = np.sqrt((x-i)**2 + (y-j)**2)
+    ind = np.argsort(r.flat)
+    return r.flat[ind], pmap.flat[ind]
 
 # average orthogonal scans
-pmap = np.average((pacsmap(args.obsid), pacsmap(args.obsid+1)), axis=0)
+pmap = np.average((pacsmap(args.obsid).patch(),
+                    pacsmap(args.obsid+1).patch()), axis=0)
 
 if args.debug:
     plt.plot(pmap.flat)
@@ -87,4 +103,14 @@ ax.set_axis_off()
 extent = ax.get_window_extent().transformed(plt.gcf().dpi_scale_trans.inverted())
 plt.savefig(join(figsdir, '{0}_{1}.png'.format(args.obsid, args.band)),
             bbox_inches=extent)
+plt.show()
+plt.close()
+
+r, prof = radprof(pmap)
+plt.scatter(r, prof)
+ax = plt.gca()
+ax.set_yscale('log')
+ax.set_xscale('log')
+plt.xlim(1, 10)
+plt.ylim(1e-2, 0.3)
 plt.show()
