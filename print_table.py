8624a0ddd0470269d9c2ba7cf8c4913adf00f9f1
Miguel de Val-Borro <miguel.deval@gmail.com>
2013-08-29 22:01:14 -0400

    print PACS map information

--- print_table.py
+++ print_table.py
@@ -12,6 +12,7 @@ def hierarch_key(header, value):
         if i.find('META_') > 0 and header[i] == value:
             return header[i[4:]]
 
+ms_speed = {'low': 10, 'medium': 20}
 f = open(join(paperdir, 'log.txt'), 'w')
 for obsid in obsids:
     hdus = pyfits.open(glob.glob(join(datadir, str(obsid), '*fits.gz'))[0])
@@ -25,11 +26,20 @@ for obsid in obsids:
     ins = hdus[0].header['INSTRUME']
     if ins == "PACS":
         scan_ang = "{:.0f}".format(hierarch_key(hdus[0].header, 'mapScanAngle'))
+        scan_speed = ms_speed[hierarch_key(hdus[0].header, 'mapScanSpeed')]
+        leglength = hierarch_key(hdus[0].header, 'mapScanLegLength')
+        numlegs = hierarch_key(hdus[0].header, 'mapScanNumLegs')
+        legstep = hierarch_key(hdus[0].header, 'mapScanCrossScan')
+        map_size = r"${}\times{:.1f}$".format(leglength,
+                                              numlegs*legstep/60).replace('.', r'\farcm')
+        print leglength, numlegs, legstep
     else:
-        scan_ang = ""
+        scan_ang, map_size, scan_speed = "", "", ""
     # calculate phase angle
     rh = deltadot(mid_time, filename="horizons.txt", column=4).item()
     delta = deltadot(mid_time, filename="horizons.txt", column=6).item()
     phi = deltadot(mid_time, filename="horizons.txt", column=8).item()
-    f.write("{:%Y-%m}-{:06.3f} & {} & {} & {:.1f} & {} & {:.2f} & {:.2f} & {:.2f}\\\\\n".format(mid_time,
-        frac_day(mid_time), ins, obsid, exp_min, scan_ang, rh, delta, phi))
+    f.write("{:%Y-%m}-{:06.3f} & {} & {} & {:.1f} & {} & {} & {} & "
+            "{:.2f} & {:.2f} & {:.2f}\\\\\n".format(mid_time,
+            frac_day(mid_time), ins, obsid, exp_min, scan_ang, map_size, scan_speed,
+            rh, delta, phi))
