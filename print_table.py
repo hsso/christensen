37781a5a5559beb8a9da377ba94f7a35e7d457c9
Miguel de Val-Borro <miguel.deval@gmail.com>
2014-01-08 14:17:02 -0500

    add superscript to PACS obsids

--- print_table.py
+++ print_table.py
@@ -40,6 +40,8 @@ for obsid in obsids:
     rh = deltadot(mid_time, filename="horizons.txt", column=4).item()
     delta = deltadot(mid_time, filename="horizons.txt", column=6).item()
     phi = deltadot(mid_time, filename="horizons.txt", column=8).item()
+    if obsid <= 1342186622:
+        obsid = r"{0}\textsuperscript{{\emph{{g}}}}".format(obsid)
     f.write("{:%Y-%m}-{:06.3f} & {} & {} & {:.1f} & {} & {} & {} &"
             "{:.2f} & {:.2f} & {:.2f}\\\\\n".format(mid_time,
             frac_day(mid_time), ins, obsid, exp_min, scan_ang,
