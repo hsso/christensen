845992d4ffa20168f904f6d37d67a3161fa4a078
Miguel de Val-Borro <miguel.deval@gmail.com>
2014-09-23 16:39:21 -0400

    Import PACS data into tarfile

--- tar_updp.py
+++ tar_updp.py
@@ -2,12 +2,19 @@
 
 import tarfile
 from os.path import join
-from christensen import datadir, hifi_obsid
+from christensen import datadir, figsdir, hifi_obsid, obsids
 
 with tarfile.open("christensen_updp.tar.gz", "w:gz") as tar:
+    # HIFI data
     for backend in ['WBS', 'HRS']:
         for ext in ['pdf', 'fits']:
             outfile = "{}_{}.{}".format(hifi_obsid, backend, ext)
             source_file = join(datadir, outfile)
-            tar.add(source_file, arcname=join('Christensen', 'HFI', outfile))
+            tar.add(source_file, arcname=join('Christensen', 'HIFI', outfile))
+    # PACS data
+    for obsid in obsids[::2]: # First obsid in each pair
+        for band in ['blue', 'red']:
+            outfile = "{}_{}.png".format(obsid, band)
+            tar.add(source_file, arcname=join('Christensen', 'PACS', outfile))
+
 tar.close()
